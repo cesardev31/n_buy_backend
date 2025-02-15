1960868e0fcc4bba88410452cdfaea9b34f262b8
cesar andres pereira bernal <cesarandresdev@gmail.com>
2025-02-15 11:34:48 -0500

    Update project URLs and Swagger configuration
    - Add regex support for Swagger JSON/YAML schema endpoints
    - Configure root URL to display Swagger UI
    - Add static file serving configuration
    - Improve URL routing for better API documentation access

--- n_buy_backend/urls.py
+++ n_buy_backend/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -38,7 +40,10 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     
     # Swagger URLs
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+    
+    # Ruta raíz
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
